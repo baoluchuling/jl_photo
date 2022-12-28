b42382f9b7c6709740fcf049ef9ff535307c6ce9
baoluchuling <baoluchuling@126.com>
2022-12-28 17:23:32 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -16,16 +16,16 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/SnapKit/SnapKit.git", .upToNextMajor(from: "5.0.0"))
+        .package(name: "SnapKit", url: "https://github.com/SnapKit/SnapKit.git", .upToNextMajor(from: "5.0.0"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "JLPhoto",
-            dependencies: []),
+            dependencies: ["SnapKit"]),
         .testTarget(
             name: "JLPhotoTests",
-            dependencies: ["JLPhoto"]),
+            dependencies: ["JLPhoto", "SnapKit"]),
     ]
 )
